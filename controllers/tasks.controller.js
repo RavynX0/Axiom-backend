695f3c7c9d9363fafe818727bd171e866044ef53
Henriette <uhenriett88@gmail.com>
2026-04-10 01:58:05 +0200

    Added controllers and updated routes

---
+++ controllers/tasks.controller.js
@@ -0,0 +1,72 @@
+let tasks = [];
+//POST controller
+const createTask = (req, res) => {
+    console.log("POST task controller hit");
+
+    const newTask = {
+        id: Date.now(),//unique id
+        ...req.body
+    };
+    tasks.push(newTask);
+    res.send({
+        message: "Task added successfully",
+        task: newTask,
+        allTasks: tasks
+    });
+};
+
+//GET controller
+
+const getTasks = (req, res) => {
+    console.log("GET tasks controller hit");
+
+    res.send({
+        message: "All tasks retrieved",
+        tasks: tasks
+    });
+};
+
+//DELETE controller
+
+const deleteTask = (req, res) =>{
+    const id = Number(req.params.id);
+
+    console.log("Delete task controller hit");
+
+    tasks = tasks.filter(task => task.id !== id);
+
+    res.send({
+        message: "Task deleted successfully",
+        allTasks: tasks
+    });
+};
+
+
+//UPDATE controllers )(put request)
+const updateTask = (req, res) => {
+    const id = Number(req.params.id);
+
+    console.log("UPDATE task controller hit");
+
+    tasks = tasks.map(task => {
+        if (task.id === id){
+            return {
+                ...task,
+                ...req.body
+            };
+        };
+    });
+
+    res.send({
+        message: "Task updated successfully",
+        allTasks: tasks
+    });
+
+};
+module.exports = {
+    createTask,
+    getTasks,
+    deleteTask,
+    updateTask
+
+}; 
--- routes/tasks.routes.js
+++ routes/tasks.routes.js
@@ -3,64 +3,81 @@ const express = require('express');
 const router = express.Router();
 
 
+//import controller
+const { createTask, getTasks, deleteTask, updateTask } = require('../controllers/tasks.controller');
+
 console.log("tasks get router loaded");
 
 
 
 //POST create task
 
-router.post('/', (req, res) => {
-    console.log("the tasks POST endpoint for post hit");
-
-    const newTask = req.body;//task sent from client
-    tasks.push(newTask);
-
-    res.send({
-        message: "Task added successfully",
-        task: newTask,
-        allTasks: tasks
-    });
-   
-});
-
+router.post('/', createTask);
 
 //GET all tasks
 
-router.get('/', (req, res) => {
-    console.log("tasks GET endpoint hit");
-    res.send({
-        message: "tasks fetched successfully",
-        tasks: tasks
-    });
-
-});
-
+router.get('/', getTasks);
 
 //PUT modify an existing route
-router.put('/:id', (req,res) => {
-    const id = req.params.id;
-    const updatedData = req.body;
-
-    tasks[id] = updatedData;
-
-    res.send({
-        message: "tasks updated successfully",
-        task: tasks[id],
-        allTasks: tasks
-    });
-});
+router.put('/:id', updateTask);
 
 //DELETE delete a task
-router.delete('/:id', (req,res) => {
-    const id = req.params.id;
-    const deletedTask = tasks.splice(id, 1);
+router.delete('/:id',deleteTask);
 
-    res.send({
-        message: "Task deleted successfully",
-        deletedTask: deletedTask,
-        allTasks: tasks
 
-    });
-});
 module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
